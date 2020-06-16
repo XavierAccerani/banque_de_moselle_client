d713d6478ff1684b730af71ad908c7e647b9d589
matthieutno <matthieu.tragno6@etu.univ-lorraine.fr>
2020-06-16 18:01:13 +0200

    Ajout des pages "Users" + "DetailUser"

---
+++ src/components/DetailUser.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="detail-user">
+    <h2>Détail utilisateur</h2>
+
+    <form @submit.prevent="validerForm">
+      <div class="form-group">
+        <label for="nom">Nom</label>
+        <input id="nom" type="text" class="form-control" v-model="user.nom" readonly>
+      </div>
+      <div class="form-group">
+        <label for="prenom">Prénom</label>
+        <input id="prenom" type="text" class="form-control" v-model="user.prenom" readonly>
+      </div>
+      <div class="form-group">
+        <br>
+        <strong>Rôles de l'utilisateur</strong>
+        <br><br>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox"
+                 id="redacteur" value="redacteurs" v-model="checkedRoles">
+          <label class="form-check-label" for="redacteur"> Rédacteur</label>
+          <br>
+          <input class="form-check-input" type="checkbox"
+                 id="viseur" value="viseurs" v-model="checkedRoles">
+          <label class="form-check-label" for="viseur"> Viseur</label>
+          <br>
+          <input class="form-check-input" type="checkbox"
+                 id="signataire" value="signataires" v-model="checkedRoles">
+          <label class="form-check-label" for="signataire"> Signataire</label>
+          <br>
+          <input class="form-check-input" type="checkbox"
+                 id="receptionneur" value="receptionneurs" v-model="checkedRoles">
+          <label class="form-check-label" for="receptionneur"> Réceptionneur</label>
+          <br>
+          <input class="form-check-input" type="checkbox"
+                 id="gestionnaire" value="gestionnaires" v-model="checkedRoles">
+          <label class="form-check-label" for="gestionnaire"> Gestionnaire</label>
+          <br>
+          <input class="form-check-input" type="checkbox"
+                 id="admin" value="admins" v-model="checkedRoles">
+          <label class="form-check-label" for="admin"> Administrateur</label>
+          <br>
+          <!-- Pour vérif -->
+          <span>{{checkedRoles}}</span>
+        </div>
+      </div>
+      <button type="submit" class="btn btn-success">Enregistrer</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "DetailUser",
+    props: ['user'],
+    data() {
+      return {
+        checkedRoles: []
+      }
+    },
+    watch: {
+      $route: 'getUsers'
+    },
+    methods: {
+      validerForm() {
+      },
+      async enregistrer() {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .detail-user {
+    padding: 5px;
+    border-radius: 3px;
+    background-color: aliceblue;
+  }
+</style>
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,37 +1,52 @@
 <template>
   <div class="row">
     <h1 class="col-12">Liste des utilisateurs</h1>
-  <div class="col-12">
-    <br>
-    <input id="user-search" class="col-xs-12" list="liste-users"
-           autocomplete="off" value="" placeholder="Rechercher un utilisateur">
-    <datalist id="liste-users">
-      <option v-for="user in users" :key="user.id" :value="user.nomComplet"></option>
-    </datalist>
-    <br>
-    <br>
-    <ul class="liste">
-      <li v-for="user in users" :key="user.id"
-          class="item">
-        {{user.nomComplet|capitalize}}
-      </li>
-    </ul>
-  </div>
+    <div class="col-6">
+      <br>
+      <input id="user-search" class="col-xs-12" list="liste-users" autocomplete="off"
+             value="" placeholder="Rechercher un utilisateur">
+      <datalist id="liste-users">
+        <option v-for="user in users" :key="user.id" :value="user.nomComplet"></option>
+      </datalist>
+      <button type="submit" class="btn btn-link" v-on:click="optionChoisie">Rechercher</button>
+      <br>
+      <br>
+      <ul class="liste">
+        <li v-for="user in usersPagination" :key="user.id"
+            class="item" @click.prevent="chargerUnUser(user.id)">
+          {{user.nomComplet|capitalize}}
+        </li>
+      </ul>
+      <Pagination :elements="users" :nbPages="nbPages" :changerTableau="setUsersPagination"/>
+    </div>
+    <div class="col-6">
+      <DetailUser :user="userForm" @rechargerUsers="rechargerUsers"/>
+    </div>
   </div>
 </template>
 
 <script>
+  import DetailUser from "../components/DetailUser";
+  import Pagination from "../components/Pagination";
 
   export default {
     name: "Users",
+    components: {
+      DetailUser,
+      Pagination,
+    },
     data() {
       return {
         users: [],
+        userForm: {},
+        usersPagination: [],
         nbPages: 0
       }
     },
     async mounted() {
       await this.getUsers();
+      this.usersPagination = this.users.slice(0, Pagination.PAR_PAGE);
+      this.calculerNombrePage();
     },
     methods: {
       async getUsers() {
@@ -42,6 +57,28 @@
           console.error(e);
         }
       },
+      async rechargerUsers() {
+        await this.getUsers();
+        this.calculerNombrePage();
+      },
+      async chargerUnUser(id) {
+        try {
+          const reponse = await this.$http.get('users/' + id);
+          this.userForm = reponse.data;
+        } catch (e) {
+          console.error(e);
+        }
+      },
+      optionChoisie() {
+        console.log('Pas encore implémenté...');
+      },
+      setUsersPagination: function (nouveauTableau) {
+        this.usersPagination = nouveauTableau;
+      },
+      calculerNombrePage() {
+        const nbUsers = this.users.length;
+        this.nbPages = Math.floor(nbUsers / Pagination.PAR_PAGE) + (nbUsers % Pagination.PAR_PAGE === 0 ? 0 : 1);
+      }
     }
   }
 </script>
